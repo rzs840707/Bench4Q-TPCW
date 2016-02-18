cf4d1737f61c9ea50b0b32b7140c241891f3767c
Zhen Tang <silenceofdaybreak@gmail.com>
2016-02-18 21:48:27 +0800

    EasyTesting-SUT: Entity "Author" added.

---
+++ easytesting-sut/src/main/java/easy/testing/sut/entity/Author.java
@@ -0,0 +1,55 @@
+package easy.testing.sut.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * AUTHOR Table Layout
+ * Field Name Field Definition Comments
+ * A_ID Numeric, 9 digits Unique Author ID
+ * A_FNAME Variable text, size 20 First Name of Author
+ * A_LNAME Variable text, size 20 Last Name of Author
+ * Primary Key: (A_ID)
+ */
+@Entity
+@Table(name = "ITEM")
+public class Author {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	@Id
+	@Column(name = "A_ID")
+	public int getId() {
+		return id;
+	}
+
+	protected void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "A_FNAME")
+	public String getFirstName() {
+		return firstName;
+	}
+
+	protected void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column(name = "A_LNAME")
+	public String getLastName() {
+		return lastName;
+	}
+
+	protected void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	protected Author() {
+
+	}
+}
