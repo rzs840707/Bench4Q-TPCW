eae37afa43b16fc2a5b8d3a65d62dcb7f0ad9121
Zhen Tang <silenceofdaybreak@gmail.com>
2016-02-23 09:44:21 +0800

    EasyTesting-SUT: Entity "Country" added.

---
+++ easytesting-sut/src/main/java/easy/testing/sut/entity/Country.java
@@ -0,0 +1,64 @@
+package easy.testing.sut.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * COUNTRY Table Layout
+ * Field Name	Field Definition	Comments
+ * CO_ID	Numeric, 4 digits	Unique Country ID
+ * CO_NAME	Variable text, size 50	Name of Country
+ * CO_EXCHANGE	Numeric, (6, 6) digits	Exchange rate to US Dollars
+ * CO_CURRENCY	Variable text, size 18	Name of Currency
+ */
+@Entity
+@Table(name = "COUNTRY")
+public class Country {
+	private int id;
+	private String name;
+	private double exchange;
+	private String currency;
+
+	@Id
+	@Column(name = "CO_ID")
+	public int getId() {
+		return id;
+	}
+
+	protected void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "CO_NAME")
+	public String getName() {
+		return name;
+	}
+
+	protected void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "CO_EXCHANGE")
+	public double getExchange() {
+		return exchange;
+	}
+
+	protected void setExchange(double exchange) {
+		this.exchange = exchange;
+	}
+
+	@Column(name = "CO_CURRENCY")
+	public String getCurrency() {
+		return currency;
+	}
+
+	protected void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	protected Country() {
+
+	}
+}
