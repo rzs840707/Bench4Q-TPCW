212657f73f2eaf5536c03c27e7411ee68e6b742c
Zhen Tang <silenceofdaybreak@gmail.com>
2016-03-03 11:01:56 +0800

    EasyTesting-SUT: Switch to annotation config.

---
+++ easytesting-sut/src/main/java/easy/testing/sut/config/WebConfig.java
@@ -0,0 +1,12 @@
+package easy.testing.sut.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "easy.testing.sut" })
+public class WebConfig {
+
+}
